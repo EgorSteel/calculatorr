b4195262d1009394b59609fad33d6ff4e391cedb
JavaDev <javabuilder@yandex.ru>
2018-09-21 15:19:21 +0300

    update 21.09.18

--- src/SupMath.java
+++ src/SupMath.java
@@ -1,6 +1,6 @@
 public class CalculatorApp extends Calculator {
 
-    static String hide = "0";
+    static String lastSum = "0";
     static char lastChar = '=';
     public static void main(String[] args) {
 
